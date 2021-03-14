31bdc55c3ce9f16593d90b4075dea7c45fa91367
JoniMujica <jonimujica3@hotmail.com>
2021-03-14 18:52:03 -0300

    Clase 2,Propiedades de clases, visibilidad, Creacion de objetos simples y con array

---
+++ Paradigma de Objetos/Clase02/Alumno.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    /*ESTO HACE VISIBLE O INVISIBLE CUANDO TENEMOS MAS DE 1 PROYECTO
+     * 
+     * 
+     * Visibilidad a nivel Clase:
+     * public: Se puede ver desde todos los proyectos (ENSAMBLADO)
+     * internal: Se puede ver solo del mismo proyecto
+     * private: Solo se puede ver desde la misma clase
+     */
+    class Alumno
+    {
+        /*
+         * Visibilidad a nivel miembros (Atributos y Metodos) Hay 3 tipos de visibilidad:
+         * public: Se puede ver desde cualquier lado (dentro del mismo objeto o fuera del mismo)
+         * protected (por defecto): es publico para la clase o sus hijos, y privado para afuera
+         * private: solo se puede ver en la misma clase
+         * 
+         */
+        public string nombre; //cambio la visibilidad de protected a public (visible para todos)
+        public string apellido;
+        public string documento; //lo pongo como string por si llegase a ser pasaporte (que tiene letras)
+
+        //Cursos
+        //Asistencia
+        //Pagos
+        public bool pagosAlDia;
+
+        //Metodo (Operacion)
+        string obtenerEstadoAlumno()
+        {
+            string estado;
+            if (pagosAlDia == true)
+            {
+                estado = "Tiene los pagos al dia";
+            }
+            else
+            {
+                estado = "Debe algun pago";
+            }
+            string resultado = nombre + " " + apellido + " (" + documento + ") " + estado;
+            return resultado;
+        }
+
+        public void imprimirEstadoAlumno()
+        {
+            string descripcionEstado = obtenerEstadoAlumno();
+            Console.WriteLine(descripcionEstado);
+        }
+    }
+}
---
+++ Paradigma de Objetos/Clase02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // creo un objeto Alumno (Instancia de la clase alumno)
+            Alumno alumno = new Alumno();
+            alumno.nombre = "Andres"; //me marca error por que la visibilidad de Alumno esta protegida
+            alumno.apellido = "Villa";
+            alumno.documento = "123123";
+            alumno.pagosAlDia = true;
+            alumno.imprimirEstadoAlumno();
+
+            Alumno miOtroAlumno = new Alumno();
+            miOtroAlumno.nombre = "Juan";
+            miOtroAlumno.apellido = "Perez";
+            miOtroAlumno.documento = "987654";
+            miOtroAlumno.imprimirEstadoAlumno();*/
+
+            // Alumno con Array
+            Alumno[] misAlumnos = new Alumno[3];
+            /*
+             [ ] [ ] [ ]
+              0   1   2 
+            */
+            for (int i = 0; i < misAlumnos.Length; i++)
+            {
+                Console.Write("Por favor, ingrese el nombre del aulmno: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Por favor, Ingrese el Apellido del alumno: ");
+                string apellido = Console.ReadLine();
+                Console.Write("Por favor, Ingrese el documento del alumno: ");
+                string documento = Console.ReadLine();
+
+                Alumno alumno = new Alumno();
+                alumno.nombre = nombre;
+                alumno.apellido = apellido;
+                alumno.documento = documento;
+                misAlumnos[i] = alumno;
+            }
+            for (int i = 0; i < misAlumnos.Length; i++)
+            {
+                misAlumnos[i].imprimirEstadoAlumno();
+            }
+            Console.WriteLine(misAlumnos[1].nombre);
+            Console.ReadKey();
+        }
+    }
+}
