28864cab67fd1553625dc4e87b781f70a1326f8d
JoniMujica <jonimujica3@hotmail.com>
2021-03-14 20:03:10 -0300

    relaciones de objetos con otros objetos (como relacionar y utilizar un objeto dentro de otro)

--- Paradigma de Objetos/Clase02/Program.cs
+++ Paradigma de Objetos/Clase02/Program.cs
@@ -10,49 +10,19 @@ namespace Clase02
     {
         static void Main(string[] args)
         {
-            /*
-            // creo un objeto Alumno (Instancia de la clase alumno)
             Alumno alumno = new Alumno();
-            alumno.nombre = "Andres"; //me marca error por que la visibilidad de Alumno esta protegida
+            alumno.nombre = "Andres";
             alumno.apellido = "Villa";
-            alumno.documento = "123123";
-            alumno.pagosAlDia = true;
-            alumno.imprimirEstadoAlumno();
 
-            Alumno miOtroAlumno = new Alumno();
-            miOtroAlumno.nombre = "Juan";
-            miOtroAlumno.apellido = "Perez";
-            miOtroAlumno.documento = "987654";
-            miOtroAlumno.imprimirEstadoAlumno();*/
-
-            // Alumno con Array
-            Alumno[] misAlumnos = new Alumno[3];
             /*
-             [ ] [ ] [ ]
-              0   1   2 
-            */
-            for (int i = 0; i < misAlumnos.Length; i++)
-            {
-                Console.Write("Por favor, ingrese el nombre del aulmno: ");
-                string nombre = Console.ReadLine();
-                Console.Write("Por favor, Ingrese el Apellido del alumno: ");
-                string apellido = Console.ReadLine();
-                Console.Write("Por favor, Ingrese el documento del alumno: ");
-                string documento = Console.ReadLine();
+            alumno.documento.tipoDeDocumento = "DU";  Esto me tira error ya que Documento no es una instancia objeto
+            alumno.documento.numeroDeDocumento = "21312312";*/
 
-                Alumno alumno = new Alumno();
-                alumno.nombre = nombre;
-                alumno.apellido = apellido;
-                alumno.documento.numeroDeDocumento = documento;
-                misAlumnos[i] = alumno;
-            }
-            for (int i = 0; i < misAlumnos.Length; i++)
-            {
-                misAlumnos[i].imprimirEstadoAlumno();
-                misAlumnos[i].Saludar();
-                misAlumnos[i].Saludar("Chau");
-            }
-            Console.WriteLine(misAlumnos[1].nombre);
+            //se soluciona asi , creo el objeto "Documento" y se lo asigno a alumno.documento
+            Documento documento = new Documento();
+            documento.tipoDeDocumento = "DU";
+            documento.numeroDeDocumento = "213412321";
+            alumno.documento = documento;
             Console.ReadKey();
         }
     }
