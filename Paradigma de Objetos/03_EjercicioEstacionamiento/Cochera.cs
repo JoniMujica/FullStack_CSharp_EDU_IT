e01d2f6246a96dd56a56777816a82f59876437da
JoniMujica <jonimujica3@hotmail.com>
2021-03-15 19:40:11 -0300

    ¿Como pensar en POO? ejercicio estacionamiento | Clase03

---
+++ Paradigma de Objetos/03_EjercicioEstacionamiento/Cochera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_EjercicioEstacionamiento
+{
+    class Cochera
+    {
+        private int numero;
+        private Vehiculo vehiculo;
+
+        public bool estaDisponible()
+        {
+            /*
+            bool res = false;
+            if (vehiculo != null)
+            {
+                res = true;
+            }
+            return res;*/
+
+            return (vehiculo == null) ? true : false;
+        }
+
+        public Vehiculo Egresar(string patente)
+        {
+            Vehiculo res = null;
+
+            if(estaDisponible() == true)
+            {
+                if (vehiculo.patente == patente)
+                {
+                    res = vehiculo;
+                    vehiculo = null;
+                }
+            }
+
+            return res;
+        }
+
+        public bool Ingresar(Vehiculo vehiculo)
+        {
+            bool res = false;
+
+            if (estaDisponible() == true)
+            {
+                this.vehiculo = vehiculo;
+                res = true;
+            }
+
+            return res;
+        }
+    }
+}
