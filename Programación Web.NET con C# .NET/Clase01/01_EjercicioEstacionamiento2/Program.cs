08182c4deaa6ae485c15f72aa4169203971a4590
JoniMujica <jonimujica3@hotmail.com>
2021-03-22 22:17:55 -0300

    encapsulamiento, beneficios

---
+++ Programación Web.NET con C# .NET/Clase01/01_EjercicioEstacionamiento2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_EjercicioEstacionamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vehiculo vehiculo = new Vehiculo("aa");
+            //CON LA CLASE ENCAPSULADA
+            Vehiculo miVehiculo = new Vehiculo();
+            miVehiculo.establecerPatente("AAA123");
+            Console.WriteLine(miVehiculo.obtenerPatente());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programación Web.NET con C# .NET/Clase01/01_EjercicioEstacionamiento2/Vehiculo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// ENCAPSULAMIENTO
+/// </summary>
+///
+namespace _03_EjercicioEstacionamiento
+{
+    class Vehiculo
+    {
+        private char[] patente;
+        public Vehiculo(string patente) //al string lo puedo tomar como un array de caracteres
+        {
+            establecerPatente(patente);
+        }
+
+        public void establecerPatente(string patente)
+        {
+            this.patente = new char[patente.Length];
+            for (int i = 0; i < patente.Length; i++)
+            {
+                this.patente[i] = patente[i];  //copio caracter por caracter del string patente a char[] patente 
+            }
+        }
+        public string obtenerPatente()
+        {
+            string res = "";
+            for (int i = 0; i < this.patente.Length; i++)
+            {
+                res += this.patente[i];
+            }
+            return res;
+        }
+
+        public Vehiculo() //al string lo puedo tomar como un array de caracteres
+        {
+
+        }
+    }
+}
