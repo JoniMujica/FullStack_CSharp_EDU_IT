4322b9423f7cad3101c3d798e8b03bc32d0c3371
JoniMujica <jonimujica3@hotmail.com>
2021-03-22 22:26:46 -0300

    remover los char para optimizar el codigo de las clases de char[] a string directo

--- Programación Web.NET con C# .NET/Clase01/01_EjercicioEstacionamiento2/Vehiculo.cs
+++ Programación Web.NET con C# .NET/Clase01/01_EjercicioEstacionamiento2/Vehiculo.cs
@@ -12,7 +12,8 @@ namespace _03_EjercicioEstacionamiento
 {
     class Vehiculo
     {
-        private char[] patente;
+        // private char[] patente;
+        private string patente;
         public Vehiculo(string patente) //al string lo puedo tomar como un array de caracteres
         {
             establecerPatente(patente);
@@ -20,20 +21,11 @@ namespace _03_EjercicioEstacionamiento
 
         public void establecerPatente(string patente)
         {
-            this.patente = new char[patente.Length];
-            for (int i = 0; i < patente.Length; i++)
-            {
-                this.patente[i] = patente[i];  //copio caracter por caracter del string patente a char[] patente 
-            }
+            this.patente = patente;
         }
         public string obtenerPatente()
         {
-            string res = "";
-            for (int i = 0; i < this.patente.Length; i++)
-            {
-                res += this.patente[i];
-            }
-            return res;
+            return patente;
         }
 
         public Vehiculo() //al string lo puedo tomar como un array de caracteres
